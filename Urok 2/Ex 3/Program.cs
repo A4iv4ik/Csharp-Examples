528387afb4d5fff44529b90b337a9eddab3df09c
A4iv4ik <danya.matyunin.90@mail.ru>
2022-01-14 22:12:51 +0400

    Ex 3 Urok 2

---
+++ Urok 2/Ex 3/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] ar = {11, 21, 452, 5455, 331, 45,44,678,567};
+
+int n = ar.Length;
+int find = 44;
+
+int index = 0;
+
+while(index< n)
+{
+   if(ar[index] == find)
+   {
+       Console.WriteLine(index);
+       break;
+   }
+    index++;
+}
