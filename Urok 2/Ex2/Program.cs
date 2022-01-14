5af0f65b6926a5f1d5ec7e94830b553b7e34472a
A4iv4ik <danya.matyunin.90@mail.ru>
2022-01-14 20:59:22 +0400

    Ex 2 Urok 2

---
+++ Urok 2/Ex2/Program.cs
@@ -0,0 +1,11 @@
+﻿int [] ar = {11, 21, 452, 5455, 331, 45,44,678,567};
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2>result) result = arg2;
+    if(arg3>result) result =arg3;
+    return result;
+}
+ar[0] = 12 ;
+int maxmax = Max(Max(ar[0],ar[1],ar[2]), Max(ar[3],ar[4],ar[5]),Max(ar[6],ar[7],ar[8]));
+Console.Write(maxmax);
