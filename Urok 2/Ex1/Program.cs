49987323f0f8fcf8bc8fb1af75a5feb753a81a67
A4iv4ik <danya.matyunin.90@mail.ru>
2022-01-14 20:50:29 +0400

    Example 1 Urok 2

--- Urok 2/Ex1/Program.cs
+++ Urok 2/Ex1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2>result) result = arg2;
+    if(arg3>result) result =arg3;
+    return result;
+}
+int a = 12;
+int b= 22;
+int c = 34;
+int d = 2;
+int f = 123;
+int g = 111;
+int r = 12;
+int p =99;
+int h = 90;
+//int max1 = Max(a,b,c);
+//int max2 = Max(d,f,g);
+//int max3 = Max(r,p,h);
+//int max4 =Max(max1,max2,max3);
+int max5 = Max(Max(a,b,c),Max(d,f,g),Max(r,p,h));
+Console.WriteLine(max5);
